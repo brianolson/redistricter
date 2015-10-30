882bcb4cb0a896931d3730ccbf4526241ba45d89
Brian Olson <brian.olson@gmail.com>
2015-10-30 14:11:47 -0400

    more options, documentation

--- plotlib.js
+++ plotlib.js
@@ -2,6 +2,17 @@
  * Copyright 2010 Brian Olson
  * GPLv2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html */
 
+/*
+options dict:
+'xlabels': ['min x label string', 'max x label string']
+'ylabels': ['min y label string', 'max y label string', (optional)'last y label string', (optional,repeated)[yvalue, 'y label string']...]
+'ytitle': 'y title string'
+'miny': minimum y value, otherwise derive from data
+'target': a y value for horizontal line for Cal plot
+'lineStyle': default 'red', or other canvas style string
+
+*/
+
 function PlotCommon() {};
 
 PlotCommon.prototype.setupxy = function(xy) {
@@ -39,13 +50,28 @@ PlotCommon.prototype.setup = function(canvas, xy, opt) {
 	this.setupxy(xy);
     }
 	this.ctx = canvas.getContext('2d');
+  if (opt && opt['ylabels']) {
+    this.miny_str = opt['ylabels'][0] || '';
+    this.maxy_str = opt['ylabels'][1] || '';
+    this.lasty_str = opt['ylabels'][2] || '';
+  } else {
 	this.miny_str = new String(this.miny);
-	this.miny_width = this.ctx.measureText(this.miny_str).width;
 	this.maxy_str = new String(this.maxy);
-	this.maxy_width = this.ctx.measureText(this.maxy_str).width;
 	this.lasty_str = new String(this.lasty);
+  }
+
+	this.miny_width = this.ctx.measureText(this.miny_str).width;
+	this.maxy_width = this.ctx.measureText(this.maxy_str).width;
 	this.lasty_width = this.ctx.measureText(this.lasty_str).width;
 	this.max_ystr_width = Math.max(this.maxy_width, this.miny_width, this.lasty_width);
+  if (opt && opt['ylabels'] && (opt['ylabels'].length > 3)) {
+    for (var yli = 3, yl; yl = opt['ylabels'][yli]; yli++) {
+      var yl_width = this.ctx.measureText(yl[1]).width;
+      if (yl_width > this.max_ystr_width) {
+        this.max_ystr_width = yl_width;
+      }
+    }
+  }
 	if (opt && (opt['miny'] != undefined)) {
 		this.miny = opt['miny'];
 	}
@@ -85,10 +111,7 @@ PlotCommon.prototype.axisLabels = function(opt) {
 		this.ctx.fillText(minxlabel, this.px(this.minx), this.py(this.miny));
 		this.ctx.textAlign = 'right';
 		this.ctx.fillText(maxxlabel, this.px(this.maxx), this.py(this.miny));
-	
-	if (opt && opt['ylabels']) {
-		
-	} else {
+
 		/* Label miny and maxy on right edge of graph. */
 		this.ctx.strokeStyle = '#555';
 		this.ctx.beginPath();
@@ -102,7 +125,6 @@ PlotCommon.prototype.axisLabels = function(opt) {
 		this.ctx.fillText(this.miny_str, this.px(this.maxx), this.py(this.miny));
 		this.ctx.textBaseline = 'top';
 		this.ctx.fillText(this.maxy_str, this.px(this.maxx), this.py(this.maxy));
-	}
 	if (opt && opt['ytitle']) {
 /* put y title right of right edge at center */
 		this.ctx.fillStyle = '#900';
@@ -110,13 +132,21 @@ PlotCommon.prototype.axisLabels = function(opt) {
 		this.ctx.textBaseline = 'middle';
 		this.ctx.fillText(new String(opt['ytitle']), this.px(this.maxx), this.py((this.maxy + this.miny)/2));
 	}
-	if ((this.lasty != this.miny) && (this.lasty != this.maxy)) {
+  if (opt && opt['ylabels'] && (opt['ylabels'].length > 3)) {
+    this.ctx.fillStyle = '#555';
+    this.ctx.textAlign = 'left';
+    this.ctx.textBaseline = 'middle';
+    for (var yli = 3, yl; yl = opt['ylabels'][yli]; yli++) {
+      this.ctx.fillText(yl[1], this.px(this.maxx), this.py(yl[0]));
+    }
+  }
+	if ((this.lasty != this.miny) && (this.lasty != this.maxy) && this.lasty_str) {
 		// Put the last Y value on the right edge in red.
 		this.ctx.fillStyle = '#900';
 		this.ctx.textAlign = 'left';
 		this.ctx.textBaseline = 'middle';
 		// TODO: something smart about how if lasty is close to miny or maxy, adjust text baselines so they don't clobber each other
-		this.ctx.fillText(new String(this.lasty), this.px(this.maxx), this.py(this.lasty));
+		this.ctx.fillText(this.lasty_str, this.px(this.maxx), this.py(this.lasty));
 	}
     if (opt && opt.data) {
 	this.ctx.textAlign = 'left';
