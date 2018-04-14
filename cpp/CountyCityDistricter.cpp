fa0ce87f4e8857ee924936f5ac7350f0c513a520
Brian Olson <brian.olson@gmail.com>
2018-04-14 14:29:54 -0400

    reindent by google-c-style.el

--- cpp/CountyCityDistricter.cpp
+++ cpp/CountyCityDistricter.cpp
@@ -12,93 +12,93 @@
 using std::map;
 
 class CCDistrict {
-	public:
+ public:
 };
 
 CountyCityDistricterSet::CountyCityDistricterSet(Solver* sovIn)
-	: DistrictSet(sovIn) {
-	if (sov->gd->place == NULL) {
-		fprintf(stderr, "error cannot run CountyCity solver without loaded places (cities)\n");
-		exit(1);
-	}
-	auto numPoints = sov->gd->numPoints;
-    uint32_t minIndex = 0, maxIndex = 0;
-    uint64_t* ubidLut = sov->gd->makeUbidLUT(&minIndex, &maxIndex);
+    : DistrictSet(sovIn) {
+  if (sov->gd->place == NULL) {
+    fprintf(stderr, "error cannot run CountyCity solver without loaded places (cities)\n");
+    exit(1);
+  }
+  auto numPoints = sov->gd->numPoints;
+  uint32_t minIndex = 0, maxIndex = 0;
+  uint64_t* ubidLut = sov->gd->makeUbidLUT(&minIndex, &maxIndex);
 #define countyOfIndex(i) ubidCounty(ubidLut[(i) - minIndex])
-	countyBlockIndexes = new uint32_t[numPoints];
-	cityBlockIndexes = new uint32_t[numPoints];
-	map<uint32_t, uint32_t> cityIdBlockCount;
-	map<uint32_t, uint32_t> countyIdBlockCount;
-	for(int i = 0; i < numPoints; i++) {
-		countyIdBlockCount[countyOfIndex(i)] += 1;
-        uint32_t place = sov->gd->place[i];
-        if (place == PlaceMap::INVALID_PLACE || place == 0) {
-            continue;
-        }
-		cityIdBlockCount[place] += 1;
-	}
+  countyBlockIndexes = new uint32_t[numPoints];
+  cityBlockIndexes = new uint32_t[numPoints];
+  map<uint32_t, uint32_t> cityIdBlockCount;
+  map<uint32_t, uint32_t> countyIdBlockCount;
+  for(int i = 0; i < numPoints; i++) {
+    countyIdBlockCount[countyOfIndex(i)] += 1;
+    uint32_t place = sov->gd->place[i];
+    if (place == PlaceMap::INVALID_PLACE || place == 0) {
+      continue;
+    }
+    cityIdBlockCount[place] += 1;
+  }
 
-	fprintf(stderr, "CC %zd counties %zd cities\n", countyIdBlockCount.size(), cityIdBlockCount.size());
-	numCounties = countyIdBlockCount.size();
-	counties = new CountyCity[numCounties];
-	int ci = 0;
-	uint32_t* cbip = countyBlockIndexes;
-	for (map<uint32_t, uint32_t>::const_iterator it = countyIdBlockCount.begin(); it != countyIdBlockCount.end(); ++it) {
-		uint32_t countyId = it->first;
-		uint32_t blockCount = it->second;
-		countyIdToIndex[countyId] = ci;
-		counties[ci].id = countyId;
-		counties[ci].blockIndexes = cbip;
-		cbip += blockCount;
-		ci++;
-	}
+  fprintf(stderr, "CC %zd counties %zd cities\n", countyIdBlockCount.size(), cityIdBlockCount.size());
+  numCounties = countyIdBlockCount.size();
+  counties = new CountyCity[numCounties];
+  int ci = 0;
+  uint32_t* cbip = countyBlockIndexes;
+  for (map<uint32_t, uint32_t>::const_iterator it = countyIdBlockCount.begin(); it != countyIdBlockCount.end(); ++it) {
+    uint32_t countyId = it->first;
+    uint32_t blockCount = it->second;
+    countyIdToIndex[countyId] = ci;
+    counties[ci].id = countyId;
+    counties[ci].blockIndexes = cbip;
+    cbip += blockCount;
+    ci++;
+  }
 
-	numCities = cityIdBlockCount.size();
-	cities = new CountyCity[numCities];
-	ci = 0;
-	cbip = cityBlockIndexes;
-	for (map<uint32_t, uint32_t>::const_iterator it = cityIdBlockCount.begin(); it != cityIdBlockCount.end(); ++it) {
-		uint32_t id = it->first;
-		uint32_t blockCount = it->second;
-		cityIdToIndex[id] = ci;
-		counties[ci].id = id;
-		counties[ci].blockIndexes = cbip;
-		cbip += blockCount;
-		ci++;
-	}
+  numCities = cityIdBlockCount.size();
+  cities = new CountyCity[numCities];
+  ci = 0;
+  cbip = cityBlockIndexes;
+  for (map<uint32_t, uint32_t>::const_iterator it = cityIdBlockCount.begin(); it != cityIdBlockCount.end(); ++it) {
+    uint32_t id = it->first;
+    uint32_t blockCount = it->second;
+    cityIdToIndex[id] = ci;
+    counties[ci].id = id;
+    counties[ci].blockIndexes = cbip;
+    cbip += blockCount;
+    ci++;
+  }
 
-	for(int i = 0; i < numPoints; i++) {
-		uint32_t county = countyOfIndex(i);
-		int ci = countyIdToIndex[county];
-		counties[ci].blockIndexes[counties[ci].numBlocks] = i;
-		counties[ci].numBlocks++;
-		counties[ci].pop += sov->gd->pop[i];
-        uint32_t place = sov->gd->place[i];
-        if (place == PlaceMap::INVALID_PLACE || place == 0) {
-            continue;
-        }
-		cityIdBlockCount[place] += 1;
-	}
+  for(int i = 0; i < numPoints; i++) {
+    uint32_t county = countyOfIndex(i);
+    int ci = countyIdToIndex[county];
+    counties[ci].blockIndexes[counties[ci].numBlocks] = i;
+    counties[ci].numBlocks++;
+    counties[ci].pop += sov->gd->pop[i];
+    uint32_t place = sov->gd->place[i];
+    if (place == PlaceMap::INVALID_PLACE || place == 0) {
+      continue;
+    }
+    cityIdBlockCount[place] += 1;
+  }
 
-	delete [] ubidLut;
+  delete [] ubidLut;
 
-	lock = new uint8_t[numPoints];
-	solution = new POPTYPE[numPoints];
-	for (int i = 0; i < numPoints; i++) {
-		lock[i] = 0;
-		solution[i] = NODISTRICT;
-	}
-	//memset(solution, NODISTRICT, sizeof(POPTYPE[numPoints]));
+  lock = new uint8_t[numPoints];
+  solution = new POPTYPE[numPoints];
+  for (int i = 0; i < numPoints; i++) {
+    lock[i] = 0;
+    solution[i] = NODISTRICT;
+  }
+  //memset(solution, NODISTRICT, sizeof(POPTYPE[numPoints]));
 }
 
 CountyCityDistricterSet::~CountyCityDistricterSet() {
-	delete [] lock;
-	delete [] solution;
-	delete [] counties;
-	delete [] cities;
-	delete [] countyBlockIndexes;
-	delete [] cityBlockIndexes;
-	delete [] they;
+  delete [] lock;
+  delete [] solution;
+  delete [] counties;
+  delete [] cities;
+  delete [] countyBlockIndexes;
+  delete [] cityBlockIndexes;
+  delete [] they;
 }
 
 double CountyCityDistricterSet::countyCloseEnoughPopulationFraction = 0.05;
@@ -106,142 +106,142 @@ static const uint8_t COUNTY_LOCK = 0x8f;
 static const uint8_t CITY_LOCK = 0x8e;
 
 void CountyCityDistricterSet::alloc(int numDistricts) {
-	DistrictSet::alloc(numDistricts);
-	ccdstorage = new CountyCityDistrict[numDistricts];
-	for (int i = 0; i < numDistricts; i++) {
-		they[i] = ccdstorage + i;
-	}
+  DistrictSet::alloc(numDistricts);
+  ccdstorage = new CountyCityDistrict[numDistricts];
+  for (int i = 0; i < numDistricts; i++) {
+    they[i] = ccdstorage + i;
+  }
 }
 void CountyCityDistricterSet::initNewRandomStart() {
-	int dpop = popTarget();
-	POPTYPE di = 0;
-	int minCountyDistrictPop = dpop * (1.0 - countyCloseEnoughPopulationFraction);
-	int maxCountyDistrictPop = dpop * (1.0 + countyCloseEnoughPopulationFraction);
-	for (int ci = 0; ci < numCounties; ci++) {
-		if (counties[ci].pop > minCountyDistrictPop && counties[ci].pop < maxCountyDistrictPop) {
-			assert(di < districts);
-			fprintf(stderr, "set district %d to county %d\n", di, counties[ci].id);
-			counties[ci].districtLock = di;
-			for (int bi = 0; bi < counties[ci].numBlocks; bi++) {
-				auto b = counties[ci].blockIndexes[bi];
-				lock[b] = COUNTY_LOCK;
-				solution[b] = di;
-			}
-			di++;
-		}
-	}
-	// start remaining districts seeded with some core random city
-	while (di < districts) {
-		int runawayLimit = 1000;
-		while (runawayLimit > 0) {
-			auto xc = random() % numCities;
-			if (cities[xc].districtLock != NODISTRICT) {
-				runawayLimit--;
-				continue;
-			}
-			bool cityOk = true;
-			for (int bi = 0; bi < cities[xc].numBlocks; bi++) {
-				if (solution[cities[xc].blockIndexes[bi]] != NODISTRICT) {
-					cityOk = false;
-					break;
-				}
-			}
-			if (!cityOk) {
-				runawayLimit--; continue;
-			}
-			cities[xc].districtLock = di;
-			for (int bi = 0; bi < cities[xc].numBlocks; bi++) {
-				auto b = cities[xc].blockIndexes[bi];
-				lock[b] = CITY_LOCK;
-				solution[b] = di;
-			}
-			break;
-		}
-		if (runawayLimit <= 0) {
-			fprintf(stderr, "could not find a city for district %d\n", di);
-			exit(1);
-		}
-		di++;
-	}
+  int dpop = popTarget();
+  POPTYPE di = 0;
+  int minCountyDistrictPop = dpop * (1.0 - countyCloseEnoughPopulationFraction);
+  int maxCountyDistrictPop = dpop * (1.0 + countyCloseEnoughPopulationFraction);
+  for (int ci = 0; ci < numCounties; ci++) {
+    if (counties[ci].pop > minCountyDistrictPop && counties[ci].pop < maxCountyDistrictPop) {
+      assert(di < districts);
+      fprintf(stderr, "set district %d to county %d\n", di, counties[ci].id);
+      counties[ci].districtLock = di;
+      for (int bi = 0; bi < counties[ci].numBlocks; bi++) {
+        auto b = counties[ci].blockIndexes[bi];
+        lock[b] = COUNTY_LOCK;
+        solution[b] = di;
+      }
+      di++;
+    }
+  }
+  // start remaining districts seeded with some core random city
+  while (di < districts) {
+    int runawayLimit = 1000;
+    while (runawayLimit > 0) {
+      auto xc = random() % numCities;
+      if (cities[xc].districtLock != NODISTRICT) {
+        runawayLimit--;
+        continue;
+      }
+      bool cityOk = true;
+      for (int bi = 0; bi < cities[xc].numBlocks; bi++) {
+        if (solution[cities[xc].blockIndexes[bi]] != NODISTRICT) {
+          cityOk = false;
+          break;
+        }
+      }
+      if (!cityOk) {
+        runawayLimit--; continue;
+      }
+      cities[xc].districtLock = di;
+      for (int bi = 0; bi < cities[xc].numBlocks; bi++) {
+        auto b = cities[xc].blockIndexes[bi];
+        lock[b] = CITY_LOCK;
+        solution[b] = di;
+      }
+      break;
+    }
+    if (runawayLimit <= 0) {
+      fprintf(stderr, "could not find a city for district %d\n", di);
+      exit(1);
+    }
+    di++;
+  }
 }
 void CountyCityDistricterSet::initFromLoadedSolution() {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 // return err { 0: ok, else err }
 int CountyCityDistricterSet::step() {
-	int numPoints = sov->gd->numPoints;
-	assert(false); // TODO: WRITEME
+  int numPoints = sov->gd->numPoints;
+  assert(false); // TODO: WRITEME
 
 
-	// decay locks
-	for (int i = 0; i < numPoints; i++) {
-		uint8_t l = lock[i];
-		if ((l != 0) && ((l & 0x80) == 0)) {
-			lock[i] = l - 1;
-		}
-	}
-	return 0;
+  // decay locks
+  for (int i = 0; i < numPoints; i++) {
+    uint8_t l = lock[i];
+    if ((l != 0) && ((l & 0x80) == 0)) {
+      lock[i] = l - 1;
+    }
+  }
+  return 0;
 }
 
 char* CountyCityDistricterSet::debugText() {
-	assert(false); // TODO: WRITEME
-	return NULL;
+  assert(false); // TODO: WRITEME
+  return NULL;
 }
 	
 void CountyCityDistricterSet::getStats(SolverStats* stats) {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 void CountyCityDistricterSet::print(const char* filename) {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 
 void CountyCityDistricterSet::fixupDistrictContiguity() {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 
 int CountyCityDistricterSet::numParameters() {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 // Return NULL if index too high or too low.
 const char* CountyCityDistricterSet::getParameterLabelByIndex(int index) {
-	assert(false); // TODO: WRITEME
-	return NULL;
+  assert(false); // TODO: WRITEME
+  return NULL;
 }
 double CountyCityDistricterSet::getParameterByIndex(int index) {
-	assert(false); // TODO: WRITEME
-	return 0.0;
+  assert(false); // TODO: WRITEME
+  return 0.0;
 }
 void CountyCityDistricterSet::setParameterByIndex(int index, double value) {
-	assert(false); // TODO: WRITEME
+  assert(false); // TODO: WRITEME
 }
 	
 // how long should this run?
 int CountyCityDistricterSet::defaultGenerations() {
-	assert(false); // TODO: WRITEME
-	return 0;
+  assert(false); // TODO: WRITEME
+  return 0;
 }
 
 
 CountyCityDistrict::CountyCityDistrict()
-	: AbstractDistrict() {
+    : AbstractDistrict() {
 }
 CountyCityDistrict::~CountyCityDistrict() {
 }
 int CountyCityDistrict::add( Solver* sov, int n, POPTYPE dist ) {
-	// TODO: unused?
-	assert(false); // TODO: WRITEME
-	return 0;
+  // TODO: unused?
+  assert(false); // TODO: WRITEME
+  return 0;
 }
 int CountyCityDistrict::remove( Solver* sov, int n, POPTYPE dist, double x, double y, double npop ) {
-	// TODO: unused?
-	assert(false); // TODO: WRITEME
-	return 0;
+  // TODO: unused?
+  assert(false); // TODO: WRITEME
+  return 0;
 }
 double CountyCityDistrict::centerX() {
-	assert(false); // TODO: WRITEME
-	return 0.0;
+  assert(false); // TODO: WRITEME
+  return 0.0;
 }
 double CountyCityDistrict::centerY() {
-	assert(false); // TODO: WRITEME
-	return 0.0;
+  assert(false); // TODO: WRITEME
+  return 0.0;
 }
--- cpp/CountyCityDistricter.h
+++ cpp/CountyCityDistricter.h
@@ -10,80 +10,80 @@ class CountyCityDistrict;
 
 /** A city or a county */
 class CountyCity {
-	public:
-	uint32_t id; // 3 digit county or 5 digit place id
-	uint32_t numBlocks;
-	uint32_t* blockIndexes; // a pointer into a large block allocated elsewhere
-	uint32_t pop;
-	POPTYPE districtLock;
+public:
+  uint32_t id; // 3 digit county or 5 digit place id
+  uint32_t numBlocks;
+  uint32_t* blockIndexes; // a pointer into a large block allocated elsewhere
+  uint32_t pop;
+  POPTYPE districtLock;
 
-	CountyCity()
-		:id(0), numBlocks(0), blockIndexes(NULL), pop(0), districtLock(NODISTRICT)
-	{}
+CountyCity()
+    :id(0), numBlocks(0), blockIndexes(NULL), pop(0), districtLock(NODISTRICT)
+  {}
 };
 
 class CountyCityDistricterSet : public DistrictSet {
-	public:
-	CountyCityDistricterSet(Solver* sovIn);
-	virtual ~CountyCityDistricterSet();
+public:
+  CountyCityDistricterSet(Solver* sovIn);
+  virtual ~CountyCityDistricterSet();
 
-	virtual void alloc(int size);
-	virtual void initNewRandomStart();
-	virtual void initFromLoadedSolution();
-	// return err { 0: ok, else err }
-	virtual int step();
+  virtual void alloc(int size);
+  virtual void initNewRandomStart();
+  virtual void initFromLoadedSolution();
+  // return err { 0: ok, else err }
+  virtual int step();
 
-	virtual char* debugText();
+  virtual char* debugText();
 	
-	virtual void getStats(SolverStats* stats);
-	virtual void print(const char* filename);
+  virtual void getStats(SolverStats* stats);
+  virtual void print(const char* filename);
 
-	virtual void fixupDistrictContiguity();
+  virtual void fixupDistrictContiguity();
 
-	virtual int numParameters();
-	// Return NULL if index too high or too low.
-	virtual const char* getParameterLabelByIndex(int index);
-	virtual double getParameterByIndex(int index);
-	virtual void setParameterByIndex(int index, double value);
+  virtual int numParameters();
+  // Return NULL if index too high or too low.
+  virtual const char* getParameterLabelByIndex(int index);
+  virtual double getParameterByIndex(int index);
+  virtual void setParameterByIndex(int index, double value);
 	
-	// how long should this run?
-	virtual int defaultGenerations();
+  // how long should this run?
+  virtual int defaultGenerations();
 
-	// If county population is within this range of ideal, call it
-	// good enough and lock off that county as a district. e.g. 0.05
-	// on an ideal pop of 100,000 allows 95,000-105,000.
-	static double countyCloseEnoughPopulationFraction;
+  // If county population is within this range of ideal, call it
+  // good enough and lock off that county as a district. e.g. 0.05
+  // on an ideal pop of 100,000 allows 95,000-105,000.
+  static double countyCloseEnoughPopulationFraction;
 
-	private:
-	std::map<uint32_t, uint32_t> countyIdToIndex;
-	CountyCity* counties;
-	unsigned int numCounties;
-	uint32_t* countyBlockIndexes; // holds allocation of uint_32[numPoints]
-	std::map<uint32_t, uint32_t> cityIdToIndex;
-	CountyCity* cities;
-	unsigned int numCities;
-	uint32_t* cityBlockIndexes; // holds allocation of uint_32[numPoints]
+private:
+  std::map<uint32_t, uint32_t> countyIdToIndex;
+  CountyCity* counties;
+  unsigned int numCounties;
+  uint32_t* countyBlockIndexes; // holds allocation of uint_32[numPoints]
+  std::map<uint32_t, uint32_t> cityIdToIndex;
+  CountyCity* cities;
+  unsigned int numCities;
+  uint32_t* cityBlockIndexes; // holds allocation of uint_32[numPoints]
 
-	// uint8_t[numPoints], 0b1xxx_xxx hard lock or 0b0xxx_xxxx decaying thrash lock
-	uint8_t* lock;
-	// POPTYPE[numPoints]
-	POPTYPE* solution;
+  // uint8_t[numPoints], 0b1xxx_xxx hard lock or 0b0xxx_xxxx decaying thrash lock
+  uint8_t* lock;
+  // POPTYPE[numPoints]
+  POPTYPE* solution;
 
-	CountyCityDistrict* ccdstorage;
+  CountyCityDistrict* ccdstorage;
 };
 
 class CountyCityDistrict : public AbstractDistrict {
-	public:
-	CountyCityDistrict();
-	virtual ~CountyCityDistrict();
-	virtual int add( Solver* sov, int n, POPTYPE dist );
-	virtual int remove( Solver* sov, int n, POPTYPE dist, double x, double y, double npop );
-	virtual double centerX();
-	virtual double centerY();
+public:
+  CountyCityDistrict();
+  virtual ~CountyCityDistrict();
+  virtual int add( Solver* sov, int n, POPTYPE dist );
+  virtual int remove( Solver* sov, int n, POPTYPE dist, double x, double y, double npop );
+  virtual double centerX();
+  virtual double centerY();
 
-	uint32_t pop;
-	double popXSum;
-	double popYSum;
+  uint32_t pop;
+  double popXSum;
+  double popYSum;
 };
 
 #endif /* COUNTY_CITY_DISTRICTER_H */
