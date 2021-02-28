00d1ab175739f0b948d5cebb63c567a1a8a04300
Brian Olson <brian.olson@gmail.com>
2021-02-28 06:09:42 -0500

    reindent, add json out

--- python/bdistricting/generaterunconfigs.py
+++ python/bdistricting/generaterunconfigs.py
@@ -5,42 +5,53 @@ Reads legislatures.csv and writes data/??/config/*
 """
 
 import csv
+import json
+import logging
 import os
 import sys
 
-legpath_ = os.path.join(os.path.dirname(__file__), 'legislatures2010.csv')
-
 from states import codeForState
 
-def run(datadir='data', stulist=None, dryrun=False, newerthan=None):
-	congress_total = 0
-	f = open(legpath_, 'rt')
-	fc = csv.reader(f)
-	for row in fc:
-		stu = codeForState(row[0])
-		if (stulist is not None) and (stu not in stulist):
-			continue
-		configdir = os.path.join(datadir, stu, 'config')
-		if not os.path.isdir(configdir):
-			os.makedirs(configdir)
-		name_part = row[1].split()[0]
-		if name_part == 'Congress':
-			congress_total += int(row[2])
-		outname = os.path.join(configdir, name_part)
-		if newerthan and not newerthan(legpath_, outname):
-			if dryrun:
-				print('"%s" up to date' % (outname))
-			continue
-		if dryrun:
-			print('would write "%s"' % (outname))
-		else:
-			out = open(outname, 'w')
-			out.write("""datadir: $DATA/%s\ncommon: -d %s\n""" % (stu, row[2]))
-			if (row[2] == '1') or (row[2] == 1):
-				out.write('disabled\n')
-			out.close()
-	return congress_total
+logger = logging.getLogger(__name__)
+
+def run(datadir='data', stulist=None, dryrun=False, newerthan=None, csvpath=None):
+        if csvpath is None:
+                csvpath = os.path.join(os.path.dirname(__file__), 'legislatures2010.csv')
+    congress_total = 0
+    # csv is
+    # State Name, Legislative Body Name, Seats
+    f = open(csvpath, 'rt')
+    fc = csv.reader(f)
+    for row in fc:
+        stu = codeForState(row[0])
+        if (stulist is not None) and (stu not in stulist):
+            logger.error('%s: unknown state %r', csvpath, row[0])
+            continue
+        configdir = os.path.join(datadir, stu, 'config')
+        if not os.path.isdir(configdir):
+            os.makedirs(configdir)
+        name_part = row[1].split()[0]
+        if name_part == 'Congress':
+            congress_total += int(row[2])
+        outname = os.path.join(configdir, name_part)
+        if newerthan and not newerthan(csvpath, outname):
+            if dryrun:
+                print('"%s" up to date' % (outname))
+            continue
+        if dryrun:
+            print('would write "%s"' % (outname))
+        else:
+            with open(outname, 'w') as out:
+                out.write("""datadir: $DATA/%s\ncommon: -d %s\n""" % (stu, row[2]))
+                if (row[2] == '1') or (row[2] == 1):
+                    out.write('disabled\n')
+            with open(outname + '.json', 'w') as out:
+                ob = {
+                    'common':{'kwargs':{'-d':row[2]}}
+                }
+                json.dump(ob, out)
+    return congress_total
 
 if __name__ == '__main__':
-	congress_total = run(dryrun=True)
-	print('congress_total == %s, should be 435' % congress_total)
+    congress_total = run(dryrun=True)
+    print('congress_total == %s, should be 435' % congress_total)
